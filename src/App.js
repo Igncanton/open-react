d8785c2b5ff080aec977e1c3bd7d478cb78e15fe
IgnacioJC <igncanton.dev@gmail.com>
2022-08-24 10:32:29 -0300

    solution fixed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import Greeting from './components/pure/greeting.';
-import Greetingf from './components/pure/greetingF';
-import TaskListComponent from './components/container/task_list';
-import Contacto from './components/componenteA/contacto';
+import ComponenteA from './components/componenteA';
 
 function App() {
+  const contactoPrueba = {
+    nombre: 'Ignacio',
+    apellido: 'Jauregui Canton',
+    email: 'igncanton@gmail.com',
+    conectado: false,
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* Componente propio Greeting.jsx */}
-        {/* <Greeting name="Martín"></Greeting> */}
-        {/* Componente de ejemplo funcional */}
-        {/* <Greetingf name='Martín'></Greetingf> */}
-        {/* Componente de Listado de Tareas */}
-        <TaskListComponent></TaskListComponent>
-        <Contacto></Contacto>
+        <ComponenteA contacto={contactoPrueba} />
       </header>
     </div>
   );
--- src/models/levels.enum.js
+++
@@ -1,5 +0,0 @@
-export const LEVELS = {
-    NORMAL: 'normal',
-    URGENT: 'urgent',
-    BLOCKING: 'blocking'
-}
